515f3b300e23790636e56db923f673fe6612d0e9
renxian <xian1995106@gmail.com>
2025-07-01 19:19:54 +0200

    Fix import statement for get_problem in dtlz_wrappers.py

--- src/problems/dtlz_wrappers.py
+++ src/problems/dtlz_wrappers.py
@@ -1,4 +1,4 @@
-from pymoo.factory import get_problem
+from pymoo.problems import get_problem
 
 def get_dtlz(name: str, n_var: int = 10, n_obj: int = 3):
     """Return a DTLZ problem instance with the study-case spec."""
